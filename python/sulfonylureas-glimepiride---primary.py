eeb73e69d1097f2d805009a9a499adb4dfdb56a2
Martin Chapman <contact@martinchapman.co.uk>
2023-08-12 13:58:15 +0100

    Update made by Phenoflow

---
+++ python/sulfonylureas-glimepiride---primary.py
@@ -0,0 +1,25 @@
+# Victor W Zhong, Juhaeri Juhaeri, Stephen R Cole, Christian M Shay, Carolyn A Chew-Graham, Penny Gordon-Larsen, Evangelos Kontopantelis, Elizabeth J Mayer-Davis, 2023.
+
+import sys, csv, re
+
+codes = [{"code":"40365","system":"gprdproduct"},{"code":"5276","system":"gprdproduct"},{"code":"5316","system":"gprdproduct"},{"code":"5353","system":"gprdproduct"},{"code":"61311","system":"gprdproduct"},{"code":"62014","system":"gprdproduct"},{"code":"6337","system":"gprdproduct"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('sulfonylureas-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["sulfonylureas-glimepiride---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["sulfonylureas-glimepiride---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["sulfonylureas-glimepiride---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
